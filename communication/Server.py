85e489396fc8fc93a01c9d1d0499492e18a00eea
afreis <reisadryan16@gmail.com>
2023-07-14 12:57:02 -0300

    rascunho docketfile e ajustes no servidor

--- communication/Server.py
+++ communication/Server.py
@@ -6,17 +6,18 @@ class Server:
     def __init__(self, host: str, port: int):
         self.__host: str = host
         self.__port: int = port
-        self.__sock: socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__sock.bind((self.__host, self.__port))
-        self.__sock.listen()
+        self.__socket: socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket.bind((self.__host, self.__port))
+        self.__socket.listen()
 
-    def accept(self) -> socket:
-        return self.__sock.accept()
-
-    def receive(self, sock: socket) -> str:
+    @staticmethod
+    def receive(sock: socket) -> str:
         return sock.recv(1024).decode()
 
+    def accept(self) -> socket:
+        return self.__socket.accept()
+
     def close(self) -> None:
-        self.__sock.close()
+        self.__socket.close()
 
 
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import threading
+
 from communication.Client import Client
 from communication.Server import Server
 from data_structures.Graph import Graph
+
+
 #
 # graph: Graph = Graph()
 # graph.add_edge(1, 2)
@@ -22,17 +26,27 @@ from data_structures.Graph import Graph
 
 # TODO: Testar comunicação com o servidor e clientes
 
+[host, port] = ['localhost', 8090]
+
+server: Server = Server(host, port)
+client1: Client = Client(host, port)
+client2: Client = Client(host, port)
+client1.send('Hello from client 1')
+client2.send('Hello from client 2')
+
 
 def test_server() -> None:
-    server: Server = Server('localhost', 8080)
-    client1: Client = Client('localhost', 8080)
 
-    [client, address] = server.accept()
-    print("Client connected:", address)
+    while True:
+        [client, addr] = server.accept()
+        print(f'client connected {addr}\n')
 
-    client1.send("Hello, world!")
+        threading.Thread(target=loop_client_receive, args=(client, addr)).start()
 
-    print(server.receive(client))
+
+def loop_client_receive(client: Client, addr: tuple[str, int]) -> None:
+    while True:
+        print(f'client {addr} sent: {server.receive(client)}')
 
 
 test_server()
